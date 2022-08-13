ed55a148805a449729aa2925b11be3d095daf9ed
subi_chacko <subimolchacko@gmail.com>
2022-08-13 10:12:00 +0400

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,12 +7,7 @@ module.exports = {
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
-            env: {
-                PORT: 8005,
-                DATABASE: 'mongodb+srv://inmenzo-cl:kteUOuOgDa7olTAp@cluster0.jdf2z.mongodb.net/HopesNews?retryWrites=true&w=majority',
-                SECRET: 'FHREJTYIKTJFtw54ojghbjkhgihhojjGGEhtrtthhMDRETY',
-                KEY:'rigyerigbeihruvwrg5iu448t3lhjbvfdrguifsdcfkjo34tnbe',
-            },
+           
         },
     ] 
 };
